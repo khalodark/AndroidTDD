b50f9190ab1eb6722db618a8a3cb9189f9dce793
Khalid <khalidqadamani@gmail.com>
2025-01-09 05:50:51 +0300

    assertion of first screen - adding content - minimum

--- app/src/main/java/com/khalodark/androidcomposetdd/MainActivity.kt
+++ app/src/main/java/com/khalodark/androidcomposetdd/MainActivity.kt
@@ -19,29 +19,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AndroidComposeTDDTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidComposeTDDTheme {
-        Greeting("Android")
-    }
+fun MainApp(){
+    Text("Home Screen")
 }
